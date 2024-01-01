d3027317f223b2b9df3840354a2abb34e5606b27
Nick Zaitsau <nickzcv@gmail.com>
2024-01-01 12:49:16 +0300

    update home

--- src/components/home/JokesGrid.tsx
+++ src/components/home/JokesGrid.tsx
@@ -60,7 +60,7 @@ export default function JokesGrid() {
           </div>
   
           <div className="pb-10 overflow-hidden md:p-10 lg:p-0 sm:pb-0">
-            <img id="heroImg1" className="transition-all duration-300 ease-in-out hover:scale-105 lg:w-full sm:mx-auto sm:w-4/6 sm:pb-12 lg:pb-0" src="/main.svg" alt="Awesome hero page image" width="500" height="488"/>
+            <img id="heroImg1" className="lg:w-full sm:mx-auto sm:w-4/6 sm:pb-12 lg:pb-0" src="/main.svg" alt="Awesome hero page image" width="500" height="488"/>
           </div>
         </div>
 
